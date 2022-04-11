674e3210368a46eed0f866ade1a85ac38f35a3a7
Pravesh105 <41507352+Pravesh105@users.noreply.github.com>
2022-04-11 16:12:34 +0530

    Add files via upload

---
+++ Head_or_Tail.py
@@ -0,0 +1,6 @@
+import random
+random_size = random.randint(0, 1)
+if random_size == 1:
+    print("Head")
+else:
+    print("Tail")
---
+++ Password_Generator.py
@@ -0,0 +1,35 @@
+import random
+l_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+u_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Strong Password Generator!")
+n_l_letters = int(input("How many lower letters would you like in your password?\n")) 
+n_u_letters = int(input("How many upper letters would you like in your password?\n"))
+n_symbols = int(input(f"How many symbols would you like?\n"))
+n_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for char in range(1, n_l_letters + 1):
+  password_list.append(random.choice(l_letters))
+
+for char in range(1, n_u_letters + 1):
+  password_list.append(random.choice(u_letters))
+
+for char in range(1, n_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, n_numbers + 1):
+  password_list += random.choice(numbers)
+
+#print(password_list)
+random.shuffle(password_list)
+#print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
